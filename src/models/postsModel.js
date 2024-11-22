1f779fed1ccf113420aa0eac58071db6efc46b79
Felipe <felipesakaiipolito@gmail.com>
2024-11-22 08:52:53 -0400

    Dia 1,2,3

---
+++ src/models/postsModel.js
@@ -0,0 +1,8 @@
+import conectarAoBanco from "../config/dbConfig.js"
+const conexao = await conectarAoBanco(process.env.STRING_CONEXAO);
+
+export async function getTodosPosts() {
+    const db = conexao.db("imersao-backend");
+    const colecao = db.collection("posts");
+    return colecao.find().toArray();
+}
